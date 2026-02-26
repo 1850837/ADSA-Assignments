66bd9436732971f8b8bcd867d5d8f7676c4256d3
Madeleine Harris-Kinloch <maddy@computer.com>
2026-02-26 19:41:56 +1030

    Not too much

--- a1.cpp
+++ a1.cpp
@@ -7,4 +7,13 @@ int main(){
     unsigned int i1, i2;
     int b;
     std::cin >> i1 >> i2 >> b;
+
+    // Replace with correct answers
+    std::cout << i1 << " " << i2 << " " << b << "\n";
+}
+
+// School method for integer addition
+
+unsigned int addition(unsigned int one, unsigned int two, int base){
+    
 }
