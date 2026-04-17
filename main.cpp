2195be33408c1bfca28843f4ca3c5f8c48cd029b
1850837 <a1850837@adelaide.edu.au>
2026-04-17 20:18:57 +0930

    Most of insertion added, no rotation yet. Testing functions also

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -15,8 +16,15 @@ class Node{
     public:
 
         // initialisation
-        Node(int key){
-            key = key;
+        Node(int newKey){
+            key = newKey;
+            left = nullptr;
+            right = nullptr;
+            height = 0;
+        }
+
+        Node(){
+            key = -1;
             left = nullptr;
             right = nullptr;
             height = 0;
@@ -34,6 +42,18 @@ class Node{
         }
         int getHeight(){
             return height;
+        }
+        int getBalance(){
+            if (getLeft() == nullptr && getRight() == nullptr){
+                return 0;
+            }
+            if (getLeft() == nullptr){
+                return -1;
+            }
+            if (getRight() == nullptr) {
+                return 1;
+            }
+            return getLeft()->getHeight() - getRight()->getHeight();
         }
         
         // setters
@@ -57,12 +77,124 @@ class Node{
 
 // === INPUT ===
 
+void input(vector<string>* values){
+    string input = "";
+    while (cin >> input){
+        values->push_back(input);
+        if ((input == "PRE" || input == "IN") || input == "POST"){
+            break;
+        }
+    }
+
+    return;
+}
+
+// === INSERT ===
+// untested
+// always returns the root
+Node* insert(Node* current, int key){
+    
+    // base case
+    if (current == nullptr){
+        return new Node(key);
+    }
+    if (current->getKey() == -1){
+        current->setKey(key);
+        return current;
+    }
+
+    // recursive step: left
+    if (key < current->getKey()){
+        current->setLeft(insert(current->getLeft(), key));
+    }
+    // recursive step: right
+    else if (key > current->getKey()){
+        current->setRight(insert(current->getRight(), key));
+    }
+    // recursive step: key already inserted
+    else {
+        return current;
+    }
+
+    // updating heights while travelling back up
+    int newHeight;
+    if (current->getLeft() == nullptr && current->getRight() == nullptr){
+        newHeight = 1;
+    }
+    else if (current->getLeft() == nullptr){
+        newHeight = 1 + current->getRight()->getHeight();
+    }
+    else if (current->getRight() == nullptr){
+        newHeight = 1 + current->getLeft()->getHeight();
+    }
+    else{
+        newHeight = 1 + max(current->getLeft()->getHeight(), current->getRight()->getHeight());
+        current->setHeight(newHeight);
+    }
+
+    // // rebalancing
+    // int balance = current->getBalance();
+
+    // // left-left case
+    // if (balance > 1 && key < current->getLeft()->getKey()){
+    //     return rightRotate(current);
+    // }
+
+    // // right-right case
+    // if (balance < -1 && key > current->getRight()->getKey()){
+    //     return leftRotate(current);
+    // }
+
+    // // left-right case
+    // if (balance > 1 && key > current->getLeft()->getKey()){
+    //     current->setLeft(leftRotate(current->getLeft()));
+    //     return rightRotate(current);
+    // }
+
+    // // right-left case
+    // if (balance < -1 && key < current->getRight()->getKey()){
+    //     current->setRight(rightRotate(current->getRight()));
+    //     return leftRotate(current);
+    // }
+
+    // no rotation needed
+    return current;
+
+}
+
+// === TRAVERSALS ===
+void traversalPre(Node* current){
+    if (current == nullptr){
+        return;
+    }
+    cout << current->getKey() << " ";
+    traversalPre(current->getLeft());
+    traversalPre(current->getRight());
+}
 
 // === MAIN ===
 
 int main(){
+    Node* root = new Node();
 
+    // collecting input
+    vector<string> values = {};
+    input(&values);
 
+    // testing DELETE LATER
+    // for (int i = 0; i < values.size(); i++){
+    //     cout << values[i] << " - ";
+    // }
+    // cout << "\n";
+
+    // inserting the insert vals (NOT FINAL don't do it this order)
+    for (int i = 0; i < values.size(); i++){
+        if (values[i][0] == 'A'){
+            insert(root, stoi(values[i].substr(1)));
+        }
+    }
+
+    traversalPre(root);
 
     return 0;
 }
