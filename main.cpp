370e97aee96194f0929dba9fff7191cae2ee7a1a
1850837 <a1850837@adelaide.edu.au>
2026-05-28 20:29:16 +0930

    Part 1 done, untested

--- main.cpp
+++ main.cpp
@@ -1,9 +1,69 @@
 #include <vector>
 #include <fstream>
 #include <iostream>
+#include <bits/stdc++.h>
 
 // M is 0-indexed internally: M[i][j] = probability of hop from node (i+1) to node (j+1).
-double solve_part1(const std::vector<std::vector<double>>& M);
+double solve_part1(const std::vector<std::vector<double>>& M){
+
+    // Creating a vector of probs for all vertices
+    // Creating a vector of visited for all vertices (True = visited, False = not visited)
+    std::vector<double> probabilities = {1};
+    std::vector<bool> visited = {false};
+    for (int i = 1; i < M.size(); i++){
+        probabilities.push_back(-1);
+        visited.push_back(false);
+    }
+
+    // Create a while loop that continues while there are unvisited nodes
+    auto it = find(visited.begin(), visited.end(), false);
+    double current_prob = 0;
+    int current = 0;
+    while (it != visited.end()){
+
+        // Choosing a current vertex
+        // This vertex will have the highest probability while not having been visited
+        // It will always begin at the first node due to the setup
+        for (int i = 0; i < visited.size(); i++){
+            if (visited[i] == false){
+                if (probabilities[i] > current_prob){
+                    current_prob = probabilities[i];
+                    current = i;
+                }
+            }
+        }
+
+        // Check all the neighbouring verticies, calculating the new prob and updating it if it's greater
+        for (int i = 0; i < visited.size(); i++){
+
+            // not current vertext
+            if (i != current){
+                // not visited
+                if (visited[i] == false){
+                 
+                    // calculating new prob (will be 0 if not a neighbour)
+                    double prob = probabilities[current]*M[current][i];
+                    if (prob > probabilities[i]){
+                        probabilities[i] = prob;
+                    }
+
+                }
+            }
+        }
+
+        // Updating current vertex as visited
+        visited[current] = true;
+
+        it = find(visited.begin(), visited.end(), false);
+
+        // Resetting current and current_prob
+        current = 0;
+        current_prob = 0;
+    }
+
+    return probabilities.back();
+
+}
 
 // Writes exactly 3 chosen edges (1-indexed) into edges_out.
 // Returns the new max probability after zeroing those edges.
@@ -11,6 +71,7 @@ double solve_part2(const std::vector<std::vector<double>>& M,std::vector<std::pa
 
 int main() {
 
+    // reading in file
     std::ifstream matrix("matrix.txt");
 
     // checking if file opened successfully
@@ -39,12 +100,14 @@ int main() {
     matrix.close();
 
     // testing
-    for (int i = 0; i < val; i++){
-        for (int j = 0; j < val; j++){
-            std::cout << mat[i][j] << " ";
-        }
-        std::cout << "\n";
-    }
+
+    double result = solve_part1(mat);
+
+    std::cout << "Expecting: 0.648. Recieved: ";
+
+    std::cout << result;
+
+    std::cout << "\n";
 
     return 0;
 }
